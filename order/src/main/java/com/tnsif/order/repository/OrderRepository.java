61959cc0caab762ec7c46085faa5cc30968903b1
Arthi <arthiezhumalai23@gmail.com>
2025-08-09 15:29:35 +0530

    Delete order directory

--- order/src/main/java/com/tnsif/order/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.tnsif.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tnsif.order.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
